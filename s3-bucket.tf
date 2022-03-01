2b672d5e71cb2b3c70d0190e75ca589516c04ac3
Your Name <you@example.com>
2022-03-01 09:00:29 +0000

    Bahh

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "gaurav"
-  bucket_prefix = "random4141"
+  bucket_prefix = "gaurav"
   acl    = "private"
 
   versioning = {
